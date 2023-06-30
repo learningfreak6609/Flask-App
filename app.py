cd69fcc2b8bbeca4cfd0c77c9d5be14a1ffbb85f
Jatin <jk066634@gmail.com>
2023-07-01 00:30:22 +0530

    fixed some issues

--- app.py
+++ app.py
@@ -64,6 +64,6 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
-if __name__ == '__main__':
-    app.run(debug=True,port=8000)
-    #app.run()
+# if __name__ == '__main__':
+#     app.run(debug=True,port=8000)
+#     #app.run()
